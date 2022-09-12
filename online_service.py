94a83cf3a3e33452ead23679fd50784faa7b9227
Ikabl <ikbal.webx@gmail.com>
2022-09-12 23:21:43 +0600

    flask added

--- online_service.py
+++ online_service.py
@@ -1,76 +1,30 @@
 import cv2
-# import pyrebase
 import requests
 import json
-# from Messenger import MessageTemplate
 from datetime import datetime
-# from CONF import auth
-
-
-# URL = 'http://192.168.31.10:8000'
-# URL = 'https://facenet-facerecognition.herokuapp.com'
-# URL = 'https://feb5f636-2302-493e-b51a-c153b27177a6.id.repl.co'
-
-
-# with open('userInfo', 'r') as f:
-# 	adminID = f.read()
 
 
 class OnlineService:
-	def __init__(self):
-		with open('app-config.json', 'r') as openfile:
-			json_object = json.load(openfile)
-		self.recognition_server_url = json_object['server_url']
-		print("Recognition server address -", self.recognition_server_url)
-		self.user_id = json_object['user_id']
-	# self.message = MessageTemplate(adminID)
-	# firebase = pyrebase.initialize_app(auth.FIREBASE_CONF)
-	# self.storage = firebase.storage()
-	# self.db = firebase.database()
-	# self.listen()
+    def __init__(self):
+        with open('app-config.json', 'r') as openfile:
+            json_object = json.load(openfile)
+        self.recognition_server_url = json_object['server_url']
+        print("Recognition server address -", self.recognition_server_url)
+        self.user_id = json_object['user_id']
 
-	# def listen(self):
-	# 	def stream_handler(msg):
-	# 		if msg['event']=='patch':
-	# 			print(msg['data'])
-	# 			self.db.child(adminID).update({'command': '', 'received': True})
-	# 	self.listener = self.db.child(adminID).stream(stream_handler)
+    def cv2ToImage(self, frame):
+        date = datetime.now()
+        image_name = "{}.jpg".format(date.isoformat())
+        image = cv2.imencode(".jpg", frame)[1]
+        return (image_name, image.tobytes(), 'image/jpeg', {'Expires': '0'})
 
-	def cv2ToImage(self, frame):
-		date = datetime.now()
-		image_name = "{}.jpg".format(date.isoformat())
-		image = cv2.imencode(".jpg", frame)[1]
-		return (image_name, image.tobytes(), 'image/jpeg', {'Expires': '0'})
-
-	# def makeImageURL(self, image):
-	# 	img_info = self.storage.child("messenger/images/{}".format(image[0])).put(image[1])
-	# 	return self.storage.child(img_info['name']).get_url(img_info['downloadTokens'])
-
-	def recognition(self, frame, notify_admin=False):
-		image = self.cv2ToImage(frame)
-		file = {"image": image}
-		try:
-			resp = requests.post(self.recognition_server_url, files=file, data={
-		                     "user_id": self.user_id, "notify_admin": notify_admin})
-			return resp.json()
-		except requests.exceptions.ConnectionError:
-			raise Exception("Connecting to {} failed".format(
-				self.recognition_server_url))
-
-	# def unauthorized(self, image_frame=None):
-	# 	image = self.cv2ToImage(image_frame)
-	# 	resp = self.message.genericTemplate(
-	# 		title="Attention!",
-	# 		subtitle="Unknown person in your doorstep.",
-	# 		image_url=self.makeImageURL(image),
-	# 		buttons=[{'title': 'Open Door', 'action': 'OPEN'}, {'title': 'Set Alert', 'action': 'ALERT'}]
-	# 	)
-
-	# def authorized(self, image_frame=None):
-	# 	image = self.cv2ToImage(image_frame)
-	# 	resp = self.message.genericTemplate(
-	# 		title="Welcome Home!",
-	# 		subtitle="Welcome back admin.",
-	# 		image_url=self.makeImageURL(image),
-	# 		buttons=[{'title': 'Lock The Door', 'action': 'CLOSE'}]
-	# 	)
+    def recognition(self, frame, notify_admin=False):
+        image = self.cv2ToImage(frame)
+        file = {"image": image}
+        try:
+            resp = requests.post(self.recognition_server_url, files=file, data={
+                "user_id": self.user_id, "notify_admin": notify_admin})
+            return resp.json()
+        except requests.exceptions.ConnectionError:
+            raise Exception("Connecting to {} failed".format(
+                self.recognition_server_url))
--- run.py
+++ run.py
@@ -1,35 +1,48 @@
+import flask
 import cv2
 import time
 from process_image import ProcessImage
 from online_service import OnlineService
 
-VIDEO_CAPTURE = 2
+VIDEO_CAPTURE = 0
 # Image maximum blur level to send request to recognition server (less value means more blur)
 BLUR_LEVEL = 110
-MAX_ATTEMPT = 3		# Try to recognize person
-MAX_FRAME_WITHOUT_FACE = 20		# Try to recognize person
+MAX_ATTEMPT = 3		# Try to recognize a person
+MAX_FRAME_WITHOUT_FACE = 20		# Stop capturing images after n number of empty frame
+UNLOCK_TIME = 5     # Unlock time after authorized access (in second)
 
 
 print('Please wait until the system is loading...')
 
 image_process = ProcessImage(BLUR_LEVEL)
 
+app = flask(__name__)
+
+AUTHORIZED = False
+
 
 def authorized():
-  print("Authorized")
+  global AUTHORIZED
+  AUTHORIZED = True
+  print("Authorized access")
+  time.sleep(UNLOCK_TIME)
+  unauthorized()
 
 
 def unauthorized():
+  global AUTHORIZED
   print("Unauthorized")
+  AUTHORIZED = False
 
 
 def startCapture():
+  global AUTHORIZED
   print('Start video capturing...')
   service = OnlineService()
   num_of_attempt = 0
   frame_without_face = 0
   capture = cv2.VideoCapture(VIDEO_CAPTURE)
-  while num_of_attempt < MAX_ATTEMPT:
+  while num_of_attempt < MAX_ATTEMPT and not AUTHORIZED:
     success, frame = capture.read()
     if not success:
       print('Stoped capturing')
@@ -54,7 +67,7 @@ def startCapture():
 
     if frame_without_face > MAX_FRAME_WITHOUT_FACE:
       break
-    time.sleep(0.5)
+    time.sleep(0.7)
 
   print("Stop capturing videos")
   capture.release()
